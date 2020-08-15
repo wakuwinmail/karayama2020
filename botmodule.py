75d91b76c28f3765249c4e853cc0e8d72776b542
pitsu <pitsu_kyo_pro@yahoo.co.jp>
2020-08-16 08:57:15 +0900

    複数人に対応

--- botmodule.py
+++ botmodule.py
@@ -6,18 +6,34 @@ import requests
 def test(message):
     message.reply('動作確認')
 
+@listen_to('テスト')
+def Test(message):
+    message.reply('動作確認')
 
+All_contest = {}
 contest = {}
 
 @respond_to('バチャ立てて')
 def InputContest(message):
-    message.reply('参加する人はIDを入力してください')
     contestAPI = requests.get('http://codeforces.com/api/contest.list?gym=false')
     contestAPI = contestAPI.json()
     if contestAPI['status'] == 'FAILED' :
         message.reply('APIの入手に失敗しました')
         return
-    
+    contestAPI = contestAPI['result']
+    for x in contestAPI:
+        if x['phase'] != 'FINISHED' :
+            continue
+        Name = x['name']
+        Id = x['id']
+        if Name.startswith('Codeforces Round #'):
+            # #PI、#FFを弾く
+            if Name[18].isdigit():
+                # Name = 'Codeforces Round #XXX'
+                Name = Name[0:21]
+                contest[Name] = Id
+                All_contest[Id] = Name
+    message.reply('参加する人はIDを入力してください')
 
 @respond_to('IDを入力 (.*)')
 def InputID(message,something):
@@ -37,25 +53,38 @@ def test_id(message,something):
 
     req_user = requests.get('http://codeforces.com/api/user.status?handle='+something+'&from=1')
     data_user = req_user.json()
-    len_user = len(data_user['result'])
+    data_user = data_user['result']
+    # len_user = len(data_user['result'])
 
-    req_contests = requests.get('https://codeforces.com/api/contest.list?gym=false')
-    data_contests = req_contests.json()
-    len_contests = len(data_contests['result'])
-    contest_names = {}
-    id_lists = []
+    # req_contests = requests.get('https://codeforces.com/api/contest.list?gym=false')
+    # data_contests = req_contests.json()
+    # len_contests = len(data_contests['result'])
+    # contest_names = {}
+    # id_lists = []
 
-    for i in range(len_contests): 
-        contest = data_contests['result'][i]
-        if contest['phase'] == 'FINISHED':
-            contest_names[contest['id']] = contest['name']
-            id_lists.append(contest['id'])
+    # for i in range(len_contests): 
+    #     contest = data_contests['result'][i]
+    #     if contest['phase'] == 'FINISHED':
+    #         contest_names[contest['id']] = contest['name']
+    #         id_lists.append(contest['id'])
     
-    for i in range(len_user):
-        check = data_user['result'][i]['contestId']
-        if check in contest_names:
-            del contest_names[check]
-            id_lists.remove(check)
+    for x in data_user:
+        check = x['contestId']
+        if check in All_contest:
+            check = All_contest[check]
+            if check.startswith('Codeforces Round #'):
+                check = check[0:21]
+                if check in contest:
+                    del contest[check]
+                    # id_lists.remove(check)
 
-    for i in range(10):
-        message.reply(contest_names[id_lists[i]])
+@respond_to('ID入力終了')
+def test_recommendation(message):
+    message.reply('テスト')
+    cnt = 0
+    for x in contest:
+        message.reply(x)
+        message.reply('http://codeforces.com/contest/'+str(contest[x]))
+        cnt += 1
+        if cnt == 3:
+            break
