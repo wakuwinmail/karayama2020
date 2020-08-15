defaa27a79ecd3163e1280f28af5ad3a70845316
N_hara <46952457+Nhara777@users.noreply.github.com>
2020-08-15 16:52:13 +0900

    Update botmodule.py
    リコメンドテスト (ユーザー名)
    でユーザーがsubmitしていないコンテストの一覧(とりあえず10件)を表示
    (終了済みのコンテストに限る)
    Div1,Div2の問題は未解決

--- botmodule.py
+++ botmodule.py
@@ -16,3 +16,32 @@ def test_id(message,something):
         return
     submission = submission['result']
     message.reply(str(len(submission)))
+
+@listen_to('リコメンドテスト (.*)')
+def test_id(message,something):
+    message.reply('テスト')
+
+    req_user = requests.get('http://codeforces.com/api/user.status?handle='+something+'&from=1')
+    data_user = req_user.json()
+    len_user = len(data_user['result'])
+
+    req_contests = requests.get('https://codeforces.com/api/contest.list?gym=false')
+    data_contests = req_contests.json()
+    len_contests = len(data_contests['result'])
+    contest_names = {}
+    id_lists = []
+
+    for i in range(len_contests): 
+        contest = data_contests['result'][i]
+        if contest['phase'] == 'FINISHED':
+            contest_names[contest['id']] = contest['name']
+            id_lists.append(contest['id'])
+    
+    for i in range(len_user):
+        check = data_user['result'][i]['contestId']
+        if check in contest_names:
+            del contest_names[check]
+            id_lists.remove(check)
+
+    for i in range(10):
+        message.reply(contest_names[id_lists[i]])
